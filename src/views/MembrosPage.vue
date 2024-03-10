649e653bcc08924cbd042b3df584df9620502d92
Henok Teixeira <henokteixeira@gmail.com>
2024-03-10 18:19:32 -0300

    feat: add modal componenet to membros page

--- src/views/MembrosPage.vue
+++ src/views/MembrosPage.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import Modal from '../components/Modal.vue';
+import { ref } from 'vue';
+
+const dialog = ref(false);
 </script>
 
 <template>
     <div class="membros">
         <h1>Membros</h1>
+        <v-btn color="info" @click="dialog = true">Adicionar novo membro</v-btn>
+
+        <Modal :isActivate="dialog" @close="dialog = false"/>
     </div>
 </template>
