d1d53920f753957989ad50af253cf0213caafb3e
Henok Teixeira <henokteixeira@gmail.com>
2024-03-10 18:18:33 -0300

    feat: add navigation bar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,37 @@
+<script setup="ts">
+const navItens = [
+    { 
+        title: "Dashboard",
+        props: {
+            to: "/",
+            prependIcon: "mdi-view-dashboard"
+        }
+    }, 
+    { 
+        title: "Membros",
+        props: {
+            to: "membros",
+            prependIcon: "mdi-account-group"
+        }
+    }, 
+    { 
+        title: "Diáconos",
+        props: {
+            to: "diaconos",
+            prependIcon: "mdi-account-supervisor"
+        } 
+    }
+];
+</script>
+
+<template>
+    <v-navigation-drawer
+        theme="dark"
+    >
+        <v-list-item 
+            height="150" 
+            title="Gerenciador de membros" 
+        />
+        <v-list :items="navItens" />
+    </v-navigation-drawer>
+</template>
