c26086169ae710d34f55a2cc6c68230fdd03041b
Henok Teixeira <henokteixeira@gmail.com>
2024-03-11 17:25:19 -0300

    feat: add a membro infos card visualization

--- src/views/MembroPage.vue
+++ src/views/MembroPage.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+const fields = ["Apelido", "Telefone", "Endereço", "Data nascimento", "Data inclusão", "Diácono responsável"];
+</script>
+
 <template>
-    <h1>
-        Visualização detalhada do membro de id = {{ $route.params.id }}
-    </h1>
+    <v-container
+        class="h-screen d-flex align-center"
+    >
+        <v-card
+            class="h-auto px-12 pb-13 pt-10 rounded-lg"
+        >
+            <v-card-title 
+                class="text-h4 mb-8 w-100 text-center"
+            >
+                Fulano de Tal | id: {{ $route.params.id }}
+            </v-card-title>
+            
+            <v-row>
+                <v-col
+                    cols="6"
+                    v-for="item in fields"
+                    :key="item"
+                >
+                    <v-card
+                        :subtitle="item"
+                        :variant="'tonal'"
+                        class="rounded-lg"
+                    >
+                        <v-card-text 
+                            class="text-h6"
+                        >
+                            {{ item }}
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-container>
 </template>
