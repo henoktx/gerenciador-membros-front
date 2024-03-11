44f0a7e7fa5423251feb1f5093c5f74ca53448e8
Henok Teixeira <henokteixeira@gmail.com>
2024-03-11 17:26:18 -0300

    feat: add router push test for membro details view

--- src/views/MembrosPage.vue
+++ src/views/MembrosPage.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
+import { router } from '@/router';
 import Modal from '../components/Modal.vue';
 import { ref } from 'vue';
 
 const dialog = ref(false);
+function teste() {
+    router.push({ name: "Membro", params: { id: 1 } });
+}
 </script>
 
 <template>
-    <div class="membros">
+    <v-container>
         <h1>Membros</h1>
         <v-btn color="info" @click="dialog = true">Adicionar novo membro</v-btn>
 
-        <Modal :isActivate="dialog" @close="dialog = false"/>
-    </div>
+        <Modal :isActivate="dialog" @close=" () => { dialog = false; teste() }"/>
+    </v-container>
 </template>
