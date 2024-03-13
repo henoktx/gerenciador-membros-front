0bcdaf214cc278570b2377270a1b59d91f4d5a53
Henok Teixeira <henokteixeira@gmail.com>
2024-03-13 10:08:54 -0300

    refactor: update modal to form modal

--- src/components/FormModal.vue
+++ src/components/FormModal.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import { getDiaconos } from '@/service/services';
+import { onBeforeMount, ref, watch } from 'vue';
+
+const diaconos = ref<Diacono[]>([]);
+
 const props = defineProps<{
-    isActivate: boolean
+    isActivate: boolean,
+    membro: Membro,
 }>();
 
-defineEmits<{"close": []}> ();
+const emit = defineEmits<{
+    (e: "operation", value: string, membro?: Membro): string,
+}> ();
+
+const membro = ref<Membro>({} as Membro);
+
+watch(props, () => {
+    membro.value = props.membro;
+});
+
+function handleConclusion() {
+    emit('operation', 'conclusion', membro.value);
+}
+
+function handleCancel() {
+    emit('operation', 'cancel');
+}
+
+onBeforeMount(async () => {
+    const result = await getDiaconos();
+
+    if (result != null) {
+        diaconos.value = result;
+    }
+})
 </script>
 
 <template>
@@ -24,78 +54,75 @@ defineEmits<{"close": []}> ();
                     class="mt-2"
                 >
                     <v-col
-                        cols="12"
-                        md="7"
-                        sm="7"
+                        cols="7"
                     >
                         <v-text-field
                             label="Nome completo*"
                             required
+                            v-model="membro.nomeCompleto"
                         ></v-text-field>
                     </v-col>
 
                     <v-col
-                        cols="12"
-                        md="5"
-                        sm="5"
+                        cols="5"
                     >
                         <v-text-field
                             label="Apelido"
+                            v-model="membro.apelido"
                         ></v-text-field>
                     </v-col>
 
                     <v-col
-                        cols="12"
-                        sm="4"
+                        cols="4"
                     >
                         <v-text-field
                             label="Nascimento*"
                             required
                             type="date"
+                            v-model="membro.dataNascimento"
                         ></v-text-field>
                     </v-col>
 
                     <v-col
-                        cols="12"
-                        sm="4"
+                        cols="4"
                     >
                         <v-text-field
                             label="Inclusão*"
                             required
                             type="date"
+                            v-model="membro.dataInclusao"
                         ></v-text-field>
                     </v-col>
 
                     <v-col
-                        cols="12"
-                        sm="4"
+                        cols="4"
                     >
                         <v-text-field
                             label="Telefone*"
                             required
+                            v-model="membro.telefone"
                         ></v-text-field>
                     </v-col>
 
                     <v-col
-                        cols="12"
-                        md="7"
-                        sm="7"
+                        cols="7"
                     >
                         <v-text-field
                             label="Endereço*"
                             required
+                            v-model="membro.endereco"
                         ></v-text-field>
                     </v-col>
 
                     <v-col
-                        cols="12"
-                        sm="5"
+                        cols="5"
                     >
-                    <v-autocomplete
-                        :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                        label="Diácono responsável"
-                        auto-select-first
-                    ></v-autocomplete>
+                        <v-autocomplete
+                            v-model="membro.diaconoResponsavel"
+                            :items="diaconos.map((m) => { return { title: m.nomeCompleto, value: m } })"
+                            label="Diácono responsável"
+                            auto-select-first
+                        ></v-autocomplete>
                     </v-col>
                 </v-row>
             </v-card-text>
@@ -108,15 +135,14 @@ defineEmits<{"close": []}> ();
                 <v-btn
                     text="Cancelar"
                     variant="plain"
-                    @click="$emit('close')"
+                    @click="handleCancel"
                 ></v-btn>
                 
                 <v-btn 
-                    @click="$emit('close')"
+                    text="Concluir"
                     color="primary"
-                    >
-                        Concluir
-                    </v-btn>
+                    @click="handleConclusion"
+                ></v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
