2d7c6e051fb0940f50194e135945a5810eb15b2a
Henok Teixeira <henokteixeira@gmail.com>
2024-03-13 10:12:15 -0300

    feat: add create membro service

--- src/views/MembrosPage.vue
+++ src/views/MembrosPage.vue
@@ -1,19 +1,95 @@
 <script setup lang="ts">
-import { router } from '@/router';
-import Modal from '../components/Modal.vue';
 import { ref } from 'vue';
+import { router } from '@/router';
+import FormModal from '../components/FormModal.vue';
+import Table from '../components/Table.vue';
+import { create, get } from '@/service/services';
+import { useDebounceFn } from '@vueuse/core';
 
-const dialog = ref(false);
-function teste() {
-    router.push({ name: "Membro", params: { id: 1 } });
+const modal = ref(false);
+const loading = ref(false);
+const search = ref("");
+const headers = ref([
+    { title: "Nome completo", value: "nomeCompleto" },
+    { title: "Diácono responsável", value: "diaconoResponsavel.nomeCompleto " },
+    { title: "Endereço", value: "endereco" },
+    { title: "Telefone", value: "telefone" },
+]);
+
+const debouncedFn = useDebounceFn((newValue) => {
+    loading.value = false;
+    search.value = newValue;
+}, 1000);
+
+async function handleModalOperation(operation: string, membro?: Membro) {
+    if (operation === "conclusion" && membro != undefined) {
+        const result = await create(membro);
+
+        if (result != null) {
+            router.push({ name: "Membro", params: { id: result.id } });
+        }
+    }
+
+    modal.value = false;
 }
 </script>
 
 <template>
-    <v-container>
-        <h1>Membros</h1>
-        <v-btn color="info" @click="dialog = true">Adicionar novo membro</v-btn>
+    <v-container 
+        class="h-screen d-flex flex-column justify-center w-75"
+    >
+        <v-card
+            class="px-12 pb-13 pt-10 rounded-lg"
+            height="700"
+        >
+            <v-card-title
+                class="text-h4 mb-8"
+            >
+                Membros
+            </v-card-title>
 
-        <Modal :isActivate="dialog" @close=" () => { dialog = false; teste() }"/>
+            <v-row>
+                <v-col
+                    cols="11"
+                    class="pl-6"
+                >
+                    <v-text-field
+                        label="Pesquisar"
+                        prepend-inner-icon="mdi-magnify"
+                        variant="outlined"
+                        hide-details
+                        clearable
+                        :loading="loading"
+                        @update:model-value="(newValue) => { loading = true; debouncedFn(newValue) }"
+                    ></v-text-field>
+                </v-col>
+
+                <v-col
+                    cols="1"
+                    class="d-flex align-center"    
+                >
+                    <v-btn
+                        color="info"
+                        class="x-large rounded h-100"
+                        icon="mdi-account-plus"
+                        @click="modal = true"
+                    ></v-btn>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <Table 
+                    :search="search" 
+                    :get-itens="get"
+                    :headers="headers"
+                />
+            </v-row>
+        </v-card>
+
+        <FormModal 
+            :isActivate="modal"
+            :membro="{} as Membro"
+            @operation="(operation: string, membro?: Membro) => { handleModalOperation(operation, membro) }"
+        />
     </v-container>
 </template>
